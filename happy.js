9227c7ab62bde8c2262423d91d7904045d00d2a9
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2017-02-20 14:09:50 -0800

    new

---
+++ happy.js
@@ -0,0 +1,11 @@
+var age = prompt("How old are you?")
+
+if(age < 21){
+  console.log("Sorry, you are not old enough")
+}
+if else(age = 21){
+  console.log("Happy 21st birthday!!")
+}
+else(age > 21){
+  console.log("your age is odd!")
+}
---
+++ poptart.js
@@ -0,0 +1,11 @@
+var age = prompt("What is your age?");
+
+if(age < 0) {
+  console.log("Come back once you're out of the womb");
+}
+if(age === 21) {
+  console.log("Happy 21st birthday!!");
+}
+if(age %  2 !==0) {
+  console.log("your age is odd!");
+}

59cda9ac2a4ab32a9c6d8d264b410a8931b5c8d6
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2018-04-07 14:08:40 -0700

    js

---
+++ Test.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Please enter first name");
+var lastName = prompt("Please enter last name");
+var Age = prompt("How old are you?");
+console.log("Your full name is " + firstName +" " + lastName+ ", and welcome to my website");
+console.log(" You age is " + Age);

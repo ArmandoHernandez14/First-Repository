c50ff47cb300224c3356c88ac613e11710c99064
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2018-04-07 14:13:33 -0700

    correction

--- Test.js
+++ Test.js
@@ -2,4 +2,4 @@ var firstName = prompt("Please enter first name");
 var lastName = prompt("Please enter last name");
 var Age = prompt("How old are you?");
 console.log("Your full name is " + firstName +" " + lastName+ ", and welcome to my website");
-console.log(" You age is " + Age);
+console.log(" Your age is " + Age);

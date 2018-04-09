9170859f1d8106c7d41943bfc1a5f0257b539ce7
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2018-04-08 22:42:04 -0700

    Javascript

---
+++ Annoy.js
@@ -0,0 +1,14 @@
+var answer = prompt("Are we there yet?");
+var rightAnswer=0;
+while(answer.indexOf("yes") === -1){
+  var answer = prompt("Are we there yet?");
+}
+alert("Yay, we made it");
+// if(answer == "yes" || answer == "yeah"){
+//   alert("Yay, we finally made it!");
+// }
+// if (answer !== "Yeah" || answer !== "yes" ) {
+//   while(5>4){
+//     alert(prompt("Are we there yet?"));
+//   }
+// }

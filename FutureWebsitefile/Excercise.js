c4b3e4701e1532d3c66af16f8d16f00c7c529fff
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2018-07-07 03:57:09 -0700

    jQuery

---
+++ FutureWebsitefile/Excercise.js
@@ -0,0 +1,8 @@
+//Selects all divs, and gives them a purple background
+$("div").css("background","purple");
+//Selects all divs with the class name highlight, and gives them a width of 200px
+$(".highlight").css("width","200px");
+//Selects the div/s with the id = "third", and gives it a orange border
+$("#third").css("border","5px solid orange");
+//Bonus: Select the first div only and changes the color to pink
+$("div:first-of-type").css("color","pink");
